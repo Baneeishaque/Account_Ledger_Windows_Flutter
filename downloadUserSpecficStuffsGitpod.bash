7d294755b311cf6ead4f4498e176666b60b20bc8
Banee Ishaque K <Baneeishaque@gmail.com>
2023-10-10 13:47:21 +0530

    Env.

---
+++ downloadUserSpecficStuffsGitpod.bash
@@ -0,0 +1,1 @@
+cd /workspace && git clone https://github.com/Baneeishaque/configurations-private.git && cd Account_Ledger_Windows_Flutter && ln -s /workspace/configurations-private/AccountLedger/.env
